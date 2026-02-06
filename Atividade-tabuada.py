b1f97fbf7a2b485fe05a978fb4349828d4675696
TiagoFernando08 <tiagofernandopereira08@gmail.com>
2026-02-06 20:15:39 -0300

    Update Atividade-tabuada.py

--- Atividade-tabuada.py
+++ Atividade-tabuada.py
@@ -4,7 +4,6 @@ while True:
  
     if tabuada < 0  or contador < 0:
         print("Para de tentar usar números negativos")
-        continue
 
     i = 0
     while i <= contador:
@@ -14,7 +13,14 @@ while True:
     print("s = Sim \n n = Não")
     decisao = input("selecione se deseja colocar uma nova tabuada").lower()
 
-    if  decisao == "n":
-        print("beleza mano ate")
+    if  decisao == "s":
+        print("calma ta reiniciando")
+         
+    elif decisao == "n":
+        print("Beleza mano até")
+        exit()
+    else :
+        print("Só tem 2 letras mano")
         break
+
 
