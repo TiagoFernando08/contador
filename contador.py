8450d6eb5b5da90951f5c7198f84e0993b4b0f7e
Tiago <tiagofernandopereira08@gmail.com>
2026-02-05 21:34:53 -0300

    contador com wihile

---
+++ contador.py
@@ -0,0 +1,23 @@
+'''
+
+O que é uma estrutura de repetição
+
+--> Serve para executar um bloco de código várias vezes
+--> ENQUANTO UMA CONDIÇÃO FOR VERDADEIRA
+'''
+conta = 5
+
+while conta >= 0:
+    print(conta)
+    conta = conta - 1
+
+
+#Tabuada
+
+tabuada = 5
+contador = 0
+
+while contador <= 10:
+    print(tabuada, "x", contador, "=", contador * tabuada)
+    contador += 1
+    
